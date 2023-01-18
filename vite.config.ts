790e48dcee55c5601589a9200c57b782c2169b41
Wojciech Marek <marek.wojciech@outlook.com>
2023-01-18 16:20:21 +0100

    feat: add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
 });
