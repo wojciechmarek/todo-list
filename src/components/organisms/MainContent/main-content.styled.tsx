dbd688463822cbd3ce9b809722aa926446074f50
Wojciech Marek <marek.wojciech@outlook.com>
2023-01-24 15:53:16 +0100

    feat: change main container max width

--- src/components/organisms/MainContent/main-content.styled.tsx
+++ src/components/organisms/MainContent/main-content.styled.tsx
@@ -7,10 +7,13 @@ export const MainContentWrapper = styled.div`
 
 export const MainContentContainer = styled.div`
   height: calc(100% - 2em);
-  width: 70%;
+  width: 90%;
   max-width: 860px;
   margin: 0.75em auto;
   border-radius: var(--standard-border-radius);
-
   background-color: var(--primary-color);
+
+  @media (max-width: 768px) {
+    width: calc(100% - 2em);
+  }
 `;
