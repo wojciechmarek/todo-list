6a35369313e4a214fdbc21d566d38e0589a36238
Wojciech Marek <marek.wojciech@outlook.com>
2023-06-12 21:13:22 +0200

    feat: apply unified style to the search input field

--- src/components/molecules/common/IconInput/icon-input.styled.tsx
+++ src/components/molecules/common/IconInput/icon-input.styled.tsx
@@ -8,6 +8,7 @@ export const IconInputContainer = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  border-radius: var(--standard-border-radius);
 
   border: none;
   height: 3em;
@@ -16,16 +17,20 @@ export const IconInputContainer = styled.div`
 export const InputStyle = styled(Input)`
   height: 100%;
   width: 100%;
+  padding-right: 4em;
+  box-shadow: var(--button-box-shadow);
 `;
 
 export const IconStyle = styled(Icon)`
   position: absolute;
-  right: 0;
+  right: 0.5em;
   height: 100%;
   width: 3em;
-  color: var(--search-magnifier-color);
-  background-color: var(--search-input-border-and-icon-bg-color);
-  border-radius: 0 var(--standard-border-radius) var(--standard-border-radius) 0;
+  border-radius: 0 var(--standard-border-radius);
   box-sizing: border-box;
   padding: 0.25em;
+
+  svg {
+    stroke: var(--search-magnifier-color);
+  }
 `;
